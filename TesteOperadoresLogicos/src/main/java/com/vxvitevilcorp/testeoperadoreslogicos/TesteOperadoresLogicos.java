fa1ea0a9abd2385594601463f82f069426c6e417
Vitorino.Vinicius <viniciusvitorinodossantos@gmail.com>
2025-01-08 01:10:42 -0300

    Add files via upload

---
+++ TesteOperadoresLogicos/src/main/java/com/vxvitevilcorp/testeoperadoreslogicos/TesteOperadoresLogicos.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.vxvitevilcorp.testeoperadoreslogicos;
+
+/**
+ *
+ * @author vxvit
+ */
+public class TesteOperadoresLogicos {
+
+    public static void main(String[] args) {
+        int x, y, z;
+        x = 4;
+        y = 7;
+        z = 12;
+        boolean r;
+        r = (x<y && y<z)?true:false;
+        System.out.println(r);
+    }
+}
