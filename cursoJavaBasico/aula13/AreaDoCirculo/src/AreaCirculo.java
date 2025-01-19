047ec7e7a9e454af02f9ae9dc3450b3462326981
Vitorino.Vinicius <viniciusvitorinodossantos@gmail.com>
2025-01-19 20:17:52 -0300

    Criando a classe e chamando no main

---
+++ cursoJavaBasico/aula13/AreaDoCirculo/src/AreaCirculo.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author vxvit
+ */
+public class AreaCirculo {
+
+    public static final double PI = 3.14159;//CONSTANTE DE PI 
+
+}
