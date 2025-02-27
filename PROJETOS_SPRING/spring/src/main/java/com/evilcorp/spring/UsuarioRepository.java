4a4006bf6e555d15474f662707c6f2bbb0ce51ae
Vitorino.Vinicius <viniciusvitorinodossantos@gmail.com>
2025-02-26 22:02:44 -0300

    spring

--- PROJETOS_SPRING/spring/src/main/java/com/evilcorp/spring/UsuarioRepository.java
+++
@@ -1,6 +0,0 @@
-package com.evilcorp.spring;
-
-public class UsuarioRepository implements InterfaceRepository {
-    @override
-    public void obterDados();
-}
--- PROJETOS_SPRING/spring/src/main/java/com/evilcorp/spring/UsuarioRepositoryAPI.java
+++
@@ -1,8 +0,0 @@
-package com.evilcorp.spring;
-
-public class UsuarioRepositoryAPI imlements interfaceRepository{
-    @override
-    public void obterDados(){
-        System.out.println("Obtendo dados da API");
-    }
-}
