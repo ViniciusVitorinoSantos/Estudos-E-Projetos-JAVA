f1d3bf347299605d946b05eb86b5ad1ee418fc5a
Vitorino.Vinicius <viniciusvitorinodossantos@gmail.com>
2025-03-02 03:13:23 -0300

    main

---
+++ For/src/pkgfor/For.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkgfor;
+
+/**
+ *
+ * @author vxvit
+ */
+public class For {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Estou sem paciência o GitHub me tirou toda ela hoje!!!");
+        
+        for(int i = 0; i < 10; ++i){
+            System.out.println("merda de atualização do GitHub " + i);
+        }
+        
+    }
+    
+}
