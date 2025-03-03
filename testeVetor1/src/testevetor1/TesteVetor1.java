80f351b05dcb2f9032b15f879858397db96d2976
Vitorino.Vinicius <viniciusvitorinodossantos@gmail.com>
2025-03-02 22:48:27 -0300

    Outra forma de declarar o vetor caso eu ja saiba as posições!

---
+++ testeVetor1/src/testevetor1/TesteVetor1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testevetor1;
+import java.util.Arrays;
+/**
+ *
+ * @author vxvit
+ */
+public class TesteVetor1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n[] = {3,5,8,2};
+        System.out.println(Arrays.toString(n));
+    }
+    
+}
