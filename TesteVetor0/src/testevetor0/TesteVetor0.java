ddfe948fcc42b81f97c0cbc5aa920b6e1b259eb8
Vitorino.Vinicius <viniciusvitorinodossantos@gmail.com>
2025-03-02 22:20:22 -0300

    Declaração vetor 0 Curso em Video

---
+++ TesteVetor0/src/testevetor0/TesteVetor0.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testevetor0;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author vxvit
+ */
+public class TesteVetor0 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        int n[] = new int[4];
+
+        n[0] = 1;
+        n[1] = 2;
+        n[2] = 3;
+        n[3] = 4;
+
+        System.out.println(Arrays.toString(n));
+
+    }
+
+}
