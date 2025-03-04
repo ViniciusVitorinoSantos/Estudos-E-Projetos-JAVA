18c0879e76d50027ebbf3b0277e8846a0b771646
Vitorino.Vinicius <viniciusvitorinodossantos@gmail.com>
2025-03-04 11:25:41 -0300

    vetorers

---
+++ testeVetor2/src/testevetor2/TesteVetor2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testevetor2;
+
+/**
+ *
+ * @author vxvit
+ */
+public class TesteVetor2 {
+    public static void main(String[] args) {
+        int n[] = {2,4,7,9,10};
+        for (int c= 0; c<=4; c++){
+            System.out.println("Na posição " + c + "temos o valor " + n[c]);
+        }
+    }
+
+}
