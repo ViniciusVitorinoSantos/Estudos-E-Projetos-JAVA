e299e7de1f972031b0953a1c3152f705d97012dd
Vitorino.Vinicius <viniciusvitorinodossantos@gmail.com>
2025-03-06 21:27:38 -0300

    teste dos metodos em java

---
+++ JavaApplication14/src/javaapplication14/JavaApplication14.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication14;
+
+/**
+ *
+ * @author vxvit
+ */
+public class JavaApplication14 {
+    
+    static int soma(int a, int b) {
+        int s = a + b;
+        return s;
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int sm = soma(5,5);
+        System.out.println(sm);
+    }
+
+}
