662e84718d3ef90f4c82f1a31f50fbb0b0886fde
Vitorino.Vinicius <viniciusvitorinodossantos@gmail.com>
2025-03-07 09:42:38 -0300

    metodos

---
+++ tstFunction02/src/tstfunction02/TstFunction02.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tstfunction02;
+
+/**
+ *
+ * @author vxvit
+ */
+public class TstFunction02 {
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here]
+        System.out.println("Vai começar a contagem !");
+        System.out.println(Operacoes.contador(1,20));
+    }
+    
+}
